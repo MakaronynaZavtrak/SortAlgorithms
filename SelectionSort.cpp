6cbdbc26d7308595ad3168897a8f02a37ad2037f
Oleg Semenov <114348027+MakaronynaZavtrak@users.noreply.github.com>
2024-07-01 06:44:33 +0300

    Create SelectionSort.cpp

---
+++ SelectionSort.cpp
@@ -0,0 +1,12 @@
+void SelectionSort(int* arr, int n)
+{
+	for (int i = 0; i < n - 1; i++)
+	{
+		int minIdx = i;
+		for (int j = i + 1; j < n; j++)
+			if (arr[j] < arr[minIdx])
+				minIdx = j;
+		if (minIdx != i)
+			Swap(arr[minIdx], arr[i]);
+	}
+}
